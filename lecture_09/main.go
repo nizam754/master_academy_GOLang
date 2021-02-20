c5e49f0c2ea5d96207399ca8430125ea1cbf9224
nizam754 <m.nizam754@outlook.com>
2021-02-20 19:08:02 +0600

    lecture-13

---
+++ lecture_09/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+var name string 
+var age int 
+var result float32
+var chr rune
+var isFound bool
+
+
+  fmt.Println(name)
+  fmt.Println(age)
+  fmt.Println(result)
+  fmt.Println(chr)
+  fmt.Println(isFound)
+  fmt.Println(true && true)
+  fmt.Println(true && false)
+  fmt.Println(true || true)
+  fmt.Println(true || false)
+  fmt.Println(!true)
+}
---
+++ lecture_11/main.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+var w1 Weight
+
+
+func main(){
+
+w1=30.50
+fmt.Println(w1, name)
+
+}
